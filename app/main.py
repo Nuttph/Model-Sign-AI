b29809979a9a2708ae269c5d0748c35fc79903e7
Nuttaphon <nathn2420@gmail.com>
2025-07-27 13:28:13 +0700

    allow

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes import lstm
-from app.config import ALLOWED_ORIGINS
+# from app.config import ALLOWED_ORIGINS
 
 app = FastAPI()
 
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ALLOWED_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
